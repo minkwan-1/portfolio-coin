a3e417419a882a92fda84b437397ee75a03783b1
원민관 <117360950+minkwan-1@users.noreply.github.com>
2023-11-26 18:16:01 +0900

    폴더 구조 변경

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
-import theme from "./theme";
+import theme from "./lib/mui/theme";
 import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
