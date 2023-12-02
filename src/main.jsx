f317a071a49a143e5a2f6c28fd691f717bfd36a6
원민관 <117360950+minkwan-1@users.noreply.github.com>
2023-12-02 15:51:54 +0900

    병합: main branch

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import * as React from "react";
 import { createRoot } from "react-dom/client";
 import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
 import App from "./App";
-import theme from "./lib/mui/theme";
+
 import { BrowserRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
@@ -12,11 +11,8 @@ const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
+      <CssBaseline />
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
